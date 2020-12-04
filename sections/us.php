d73bec9aa43150c8e52a8f2d75c2ca7ce703a470
jude okoroafor <judeokoroafor@gmail.com>
2020-12-04 17:52:46 +0100

    Update us.php

--- sections/us.php
+++ sections/us.php
@@ -18,7 +18,7 @@
     <section class="about-us aboutus-3 my-sm-5">
         <div class="container">
             <div class="row">
-                <div class="col-sm-5 my-auto offset-sm-1">
+                <div class="col-md-6 col-sm-7 my-auto offset-md-1">
                     <div class="">
                         <p class="lead text-right py-4">With a professional career spanning 15 years, Zara Odu, an award winning communications expert, has been at the forefront of the growth and development of the apparel industry, working to grow and build brands across Africa. </p>
                         <p class="lead text-right py-4">In 2017, she launched Designers Consociate, a boutique firm committed to developing sustainable solutions that invigorate the African apparel eco system.</p>
