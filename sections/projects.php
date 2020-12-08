41da1fe9d64577209ee849ec966a2cb1ff686b51
jude okoroafor <judeokoroafor@gmail.com>
2020-12-08 14:20:04 +0100

    Update projects.php

--- sections/projects.php
+++ sections/projects.php
@@ -19,23 +19,48 @@
             <div class="justify-content-around row ">
 
                 <div class="col-sm-6 project1 p-0">
-                    <div class="">
+                    <div class="my-5 mx-5">
                         <span class="text-muted">Project 1</span>
                         <h2>The Designer Shirt Project</h2>
                         <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC)</p>
                         <div>
-                            <div class="text-center project">
-                                <img src="img/HCDC-6-edit1.jpg" alt="" class="img-fluid rounded">
-                                <img src="img/img25.jpg" alt="" class="img-fluid rounded">
-                            </div>
-                            <div class="project ">
-                                <img src="img/HCDC-6-edit1.jpg" alt="Sketch project" class="image img-fluid">
-                                <div class="overlay">
-                                    <div class="text">John Doe</div>
+                            <div class="text- project row">
+                                <div class="py-2 col-6">
+                                    <img src="img/HCDC-6-edit1.jpg" alt="" class="img-fluid rounded ">
                                 </div>
-                                <img src="img/HCDC-6-edit1.jpg" alt="Sketch project" class="image img-fluid">
-                                <div class="overlay">
-                                    <div class="text">John Doe</div>
+                                <div class="py-2 col-6">
+                                    <img src="img/img25.jpg" alt="" class="img-fluid rounded ">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-5 mx-5">
+                        <span class="text-muted">Project 1</span>
+                        <h2>The Designer Shirt Project</h2>
+                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC)</p>
+                        <div>
+                            <div class="text- project row">
+                                <div class="py-2 col-6">
+                                    <a href="#">
+
+                                        <div class="overlaycontainer ">
+                                            <img src="img/HCDC-6-edit1.jpg" alt="Avatar" class="image img-fluid">
+                                            <div class="overlay">
+                                                <div class="overlayText">Hello World</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="py-2 col-6">
+                                    <div class="overlaycontainer ">
+                                        <a href="#">
+
+                                            <img src="img/img25.jpg" alt="Avatar" class="image img-fluid ">
+                                            <div class="overlay">
+                                                <div class="overlayText">Hello World</div>
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
