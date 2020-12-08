924d8c8c621043ed080963f13c5d394ea09fa350
jude okoroafor <judeokoroafor@gmail.com>
2020-12-08 15:55:33 +0100

    Update projects.php

--- sections/projects.php
+++ sections/projects.php
@@ -16,26 +16,11 @@
 
     <section id="projects" class="projects-section ">
         <div class="container-fluid">
-            <div class="justify-content-around row ">
+            <div class="justify-content-around row  ">
 
                 <div class="col-sm-6 project1 p-0">
                     <div class="my-5 mx-5">
-                        <span class="text-muted">Project 1</span>
-                        <h2>The Designer Shirt Project</h2>
-                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC)</p>
-                        <div>
-                            <div class="text- project row">
-                                <div class="py-2 col-6">
-                                    <img src="img/HCDC-6-edit1.jpg" alt="" class="img-fluid rounded ">
-                                </div>
-                                <div class="py-2 col-6">
-                                    <img src="img/img25.jpg" alt="" class="img-fluid rounded ">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="my-5 mx-5">
-                        <span class="text-muted">Project 1</span>
+                        <span class="text-muted">Project I</span>
                         <h2>The Designer Shirt Project</h2>
                         <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC)</p>
                         <div>
@@ -44,9 +29,9 @@
                                     <a href="#">
 
                                         <div class="overlaycontainer ">
-                                            <img src="img/HCDC-6-edit1.jpg" alt="Avatar" class="image img-fluid">
+                                            <img src="/img/g22.png" alt="Avatar" class="image img-fluid">
                                             <div class="overlay">
-                                                <div class="overlayText">Hello World</div>
+                                                <div class="overlayText">2017</div>
                                             </div>
                                         </div>
                                     </a>
@@ -55,9 +40,9 @@
                                     <div class="overlaycontainer ">
                                         <a href="#">
 
-                                            <img src="img/img25.jpg" alt="Avatar" class="image img-fluid ">
+                                        <img src="/img/g3826.png" alt="Avatar" class="image img-fluid">
                                             <div class="overlay">
-                                                <div class="overlayText">Hello World</div>
+                                                <div class="overlayText">2018</div>
                                             </div>
                                         </a>
                                     </div>
@@ -67,10 +52,38 @@
                     </div>
                 </div>
                 <div class="col-sm-6 project2 project p-0">
-                    <div class=" p-4">
-                        <span class="text-muted">Project 1</span>
+                    <div class="my-5 mx-5 text-right">
+                        <span class="text-mut-ed">Project II</span>
                         <h2>The Designer Shirt Project</h2>
-                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC)</p>
+                        <p>We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work.</p>
+                        <p>We nominated:</p>
+                        <div>
+                            <div class="project row">
+                                <div class="py-2 col-6 text-right ml-auto">
+                                    <a href="#">
+                                        <div class="overlaycontainer ">
+                                            <img src="img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                            <div class="overlay">
+                                                <div class="overlayText">Studio Lani</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="py-2 col-6 text-right ml-auto">
+                                    <a href="#">
+                                        <div class="overlaycontainer ">
+                                            <img src="img/raffia-bags-edit-aaks1.jpg" alt="Avatar" class="image img-fluid">
+                                            <div class="overlay">
+                                                <div class="overlayText">
+                                                    <h4>Studio Lani</h4>
+                                                    <a href="">more...</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
