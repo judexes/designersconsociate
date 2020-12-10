4ec0803861cc7fb2abe829bacce4b927b5522aa3
jude okoroafor <judeokoroafor@gmail.com>
2020-12-10 10:34:01 +0100

    Create top-bar-2.php

---
+++ inc/top-bar-2.php
@@ -0,0 +1,38 @@
+<nav id="navbar" class="navbar navbar-white fixed-top mr-sm-5 pl-sm-3">
+    <a id="Reloadpage" class="navbar-brand mr-auto mr-lg-0 my-sm-2 mx-sm-2" href="">
+        <span class="d-none">Designers Consociate</span>
+        <img class="logo " id="logo" src="/img/designers-consociate-logo-black.svg" alt="image" class="img-fluid">
+    </a>
+    <button class="navbar-toggler p-0 border-0 mx-sm-3" type="button" data-toggle="offcanvas">
+        <img class="hamburger" src="/img/menu3black.svg"/>        
+    </button>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+        <div>
+            <div class="row mt-3">
+                <div class="col text-right">
+                    <button class="navbar-toggler p-0 border-0 mr-sm-3" type="button" data-toggle="offcanvas">
+                        <img class="close hamburger mr-sm-2 mt-sm-3 mt-2 mr-2" src="/img/cancel.svg"/>        
+                    </button>
+                </div>
+            </div>
+            <div class="navbar-nav mr-auto text-center mt-5">
+                <p class="nav-item">
+                    <a class="nav-link" href="/index.php"><span class="link-border">Home</span></a>
+                </p>
+                <p class="nav-item ">
+                    <a class="nav-link" href="/us.php"><span class="link-border">Us</span></a>
+                </p>
+                <p class="nav-item ">
+                    <a class="nav-link" href="/what-we-do.php"><span class="link-border">What We Do</span></a>
+                </p>
+                <p class="nav-item">
+                    <a class="nav-link" href="/projects.php"><span class="link-border">Our Projects</span></a>
+                </p>
+                <p class="nav-item">
+                    <a class="nav-link" href="#"><span class="link-border">Contact Us</span></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</nav>
