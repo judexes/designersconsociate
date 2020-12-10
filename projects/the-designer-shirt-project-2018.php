d8db97f2311dbdee7b94fdae4c3076cd994bfa68
jude okoroafor <judeokoroafor@gmail.com>
2020-12-10 10:34:16 +0100

    Update the-designer-shirt-project-2018.php

---
+++ projects/the-designer-shirt-project-2018.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<?php include '../inc/header.php' ?>
+
+<body class="single-project">
+
+    <?php include '../inc/top-bar-2.php' ?>
+
+    <section>
+        <div class="container hero">
+            <div class="shade">
+                <div class="row justify-content-md-center">
+                    <div class="col col-sm-10 col-md-6">
+                        <div class="text-center">
+                            <span class="lead pt-sm-0  ">Our Projects</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include '../inc/footer2.php' ?>
+
+
+</body>
+
+</html>
