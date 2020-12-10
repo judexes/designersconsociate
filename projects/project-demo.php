e2383208030f06587c3eb6668eb3abad19b05e9b
jude okoroafor <judeokoroafor@gmail.com>
2020-12-10 14:04:00 +0100

    Create project-demo.php

---
+++ projects/project-demo.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<?php include '../inc/header.php' ?>
+
+<body class="single-project">
+
+    <?php include '../inc/top-bar-2.php' ?>
+
+    <section class="hero-section ">
+        <div class="container">
+            <div class="hero">
+                <div class="shade shade view-height-50 center-children">
+                    <div class="row justify-content-center align-items-center ">
+                        <div class="">
+                            <div class="text-center">
+                                <span class="title pt-sm-0  ">Project</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="project-section mt-5">
+        <div class="container view-height-100">
+            <div class="shade">
+                <div class="row ">
+                    <div class="col-sm-6 ">
+                        <div class="p-2">
+
+                            <img src="/img/NU-MI-DESIGN.jpg" alt="image" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="z-index-99">
+                        <div class="text-center cursive p-3 title">
+                            <p class="m-0">Create Day | London Craft Week <small> <i>2017</i></small></p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 z-index-75">
+                        <div class="p-2 pt-5">
+                            <div class="">
+                                <p class="lead pt-sm-0  ">We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work.</p>
+                                <p class="lead">We nominated <small><a href="" class="link">A A K S</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="" class="link">Studio Lani</a></small></p>
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col">                                        
+                                        <img src="/img/StudioLani-white-on-black.png" alt="image" class="img-fluid brand-logo lani">
+                                    </div>
+                                    <div class="col">                                        
+                                        <img src="/img/aaks_hand-crafted-white-on-black.png" alt="image" class="img-fluid brand-logo aaks">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include '../inc/footer2.php' ?>
+
+
+</body>
+
+</html>
