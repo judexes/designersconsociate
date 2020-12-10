62498fc4caaa542d0f3ec2b89532ea39ab0ecb8d
jude okoroafor <judeokoroafor@gmail.com>
2020-12-10 10:34:13 +0100

    Update the-designer-shirt-project-2017.php

--- projects/the-designer-shirt-project-2017.php
+++ projects/the-designer-shirt-project-2017.php
@@ -1,15 +1,32 @@
 <!doctype html>
 <html lang="en">
-    
-<?php include 'inc/header.php' ?>
+
+<?php include '../inc/header.php' ?>
 
 <body class="">
 
-    <?php include 'inc/top-bar.php' ?>
+    <?php include '../inc/top-bar-2.php' ?>
 
-    <?php include 'sections/projects.php' ?>
+    <div class="container post-section">
+        <div class="row justify-content-md-center">
+            <div class="col col-sm-8 col-md-6">
+                <div class="row">
+                    <img src="/img/HCDC-6-edit1.jpg" alt="Image" class="img-fluid">
+                </div>
+                <div class="row p-4 p-sm-0">
+                    <div class="col p-0">
 
-    <?php include 'inc/footer.php' ?>
+                    <h6><span class="lead font2">Project I</span></h6>
+                    <h2 class="mb-0">The Designer Shirt Project</h2>
+                    <p class="lead font2 mb-2 mt-1">2017 / 2018</p>
+                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../inc/footer2.php' ?>
 
 
 </body>
