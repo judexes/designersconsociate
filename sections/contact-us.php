a0617bbf90f72d0171c2bd9fb93dd2763ea6e901
jude okoroafor <judeokoroafor@gmail.com>
2020-12-14 09:51:02 +0100

    Update contact-us.php

--- sections/contact-us.php
+++ sections/contact-us.php
@@ -1,12 +1,11 @@
     <!-- HERO SECTION -->
-    <section id="top" class="hero-section shade">
-        <div class="splash view-height-100 view-height-0-sm center-children center-children-0-sm">
+    <section id="top" class="hero-section  contact-us">
+        <div class=" view-height-100 view-height-0-sm center-children center-children-0-sm">
             <div class="container">
                 <div class="row pt-sm-0 justify-content-center align-items-center">
-                    <div class="col mt-sm-5">
+                    <div class="col">
                         <div class="hero-description text-center">
-                            <h1 class="title pt-5 pt-sm-0 mb-3">Sustainable Solutions</h1>
-                            <p class="description mt-1">We are committed to developing sustainable solutions that invigorate the African Fashion ecosystem</p>
+                            <a class="title" href="mailto:hello@designersconsociate.org">hello@designersconsociate.org</a> 
                         </div>
                     </div>
                 </div>
