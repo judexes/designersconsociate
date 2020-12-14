38a5a067e130768bad1b3d3d5866773bcafc9fa2
jude okoroafor <judeokoroafor@gmail.com>
2020-12-14 09:13:46 +0100

    Create contact-us.php

---
+++ sections/contact-us.php
@@ -0,0 +1,24 @@
+    <!-- HERO SECTION -->
+    <section id="top" class="hero-section shade">
+        <div class="splash view-height-100 view-height-0-sm center-children center-children-0-sm">
+            <div class="container">
+                <div class="row pt-sm-0 justify-content-center align-items-center">
+                    <div class="col mt-sm-5">
+                        <div class="hero-description text-center">
+                            <h1 class="title pt-5 pt-sm-0 mb-3">Sustainable Solutions</h1>
+                            <p class="description mt-1">We are committed to developing sustainable solutions that invigorate the African Fashion ecosystem</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row pt-2 justify-content-center">
+                    <div class="col-auto">
+                        <div class="text-center">
+                            <a href="us.php">
+                                <span class="button">us</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
