b20e71850d45e3952bcf3cc81c61e242aeb69e71
jude okoroafor <judeokoroafor@gmail.com>
2020-12-15 09:51:56 +0100

    Update contact-us.php

--- sections/contact-us.php
+++ sections/contact-us.php
@@ -1,20 +1,13 @@
     <!-- HERO SECTION -->
-    <section id="top" class="hero-section  contact-us">
-        <div class=" view-height-100 view-height-0-sm center-children center-children-0-sm">
-            <div class="container">
-                <div class="row pt-sm-0 justify-content-center align-items-center">
-                    <div class="col">
-                        <div class="hero-description text-center">
-                            <a class="title" href="mailto:hello@designersconsociate.org">hello@designersconsociate.org</a> 
-                        </div>
-                    </div>
-                </div>
-                <div class="row pt-2 justify-content-center">
-                    <div class="col-auto">
-                        <div class="text-center">
-                            <a href="us.php">
-                                <span class="button">us</span>
-                            </a>
+    <section id="top" class="  contact-us">
+        <div class="shadeZZZ">
+            <div class="bg-imageZZZ view-height-100 view-height-0-sm center-children center-children-0-sm">
+                <div class="container">
+                    <div class="row pt-sm-0 justify-content-center align-items-center">
+                        <div class="col">
+                            <div class="hero-description text-center">
+                                <a class="title" href="mailto:hello@designersconsociate.org">hello@designersconsociate.org</a>
+                            </div>
                         </div>
                     </div>
                 </div>
