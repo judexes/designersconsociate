5774f5446ed62db348498d168a565bfb94ba818d
jude okoroafor <judeokoroafor@gmail.com>
2020-12-15 09:52:35 +0100

    Update project-demo.php

--- projects/project-demo.php
+++ projects/project-demo.php
@@ -7,10 +7,10 @@
 
     <?php include '../inc/top-bar-2.php' ?>
 
-    <section class="hero-section ">
-        <div class="container">
-            <div class="hero">
-                <div class="shade shade view-height-50 center-children">
+    <section class="hero-section shade">
+        <div class="container-fluid p-0 hero ">
+            <div class="">
+                <div class=" shade view-height-50 center-children">
                     <div class="row justify-content-center align-items-center ">
                         <div class="">
                             <div class="text-center">
@@ -29,7 +29,6 @@
                 <div class="row ">
                     <div class="col-sm-6 ">
                         <div class="p-2">
-
                             <img src="/img/NU-MI-DESIGN.jpg" alt="image" class="img-fluid">
                         </div>
                     </div>
@@ -42,17 +41,20 @@
                         <div class="p-2 pt-5">
                             <div class="">
                                 <p class="lead pt-sm-0  ">We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work.</p>
-                                <p class="lead">We nominated:&nbsp;&nbsp;&nbsp;<small><a href="" class="link">A A K S</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="" class="link">Studio Lani</a></small></p>
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 style="font-family: 'Pacifico', cursive!important;">Studio lani</h5>
-                                        <p>Studio-Lani creates handmade sculptural lighting and furniture pieces that celebrate West African elegance with a contemporary sensibility, working closely with various artisans in Nigeria. </p>
-                                    </div>
-                                    <div class="col">
-                                        <h5 style="font-family: 'Pacifico', cursive!important;">A A K S</h5>
-                                        <p>AAKS introduces the world to the weaving techniques of the women of Ghana, while creating sustainable jobs in Africa. Handcrafted in Ghana, A A K S creates bags in styles in bright and exuberant colours that maintain the spirit and durability of their ancestral counterparts.</p>
-                                    </div>
-                                </div>
+                                <p class="lead">We nominated:&nbsp;&nbsp;&nbsp;<small>
+
+                                    <abbr title="AAKS introduces the world to the weaving techniques of the women of Ghana, while creating sustainable jobs in Africa. Handcrafted in Ghana, A A K S creates bags in styles in bright and exuberant colours that maintain the spirit and durability of their ancestral  counterparts.">
+                                        <a href="https://www.aaksonline.com/" class="link">A A K S</a>
+                                    </abbr>
+
+                                        &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+
+                                    <abbr title="Studio-Lani creates handmade sculptural lighting and furniture pieces that celebrate West African elegance with a contemporary sensibility, working closely with various artisans in Nigeria.">
+                                            
+                                            <a href="http://studio-lani.com/" class="link">Studio Lani</a>
+                                        
+                                    </abbr>
+                                </small></p>
                             </div>
                         </div>
                     </div>
@@ -90,7 +92,43 @@
                                         
                                     </abbr>
                                 </small></p>
-                                <abbr title="World Health Organization">WHO</abbr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container view-height-100  mt-5">
+            <div class="shade">
+                <div class="row ">
+                    <div class="col-sm-6 ">
+                        <div class="p-2">
+                            <img src="/img/NU-MI-DESIGN.jpg" alt="image" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="z-index-99">
+                        <div class="text-center cursive p-3 title">
+                            <p class="m-0">Create Day | London Craft Week <small> <i>2017</i></small></p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 z-index-75">
+                        <div class="p-2 pt-5">
+                            <div class="">
+                                <p class="lead pt-sm-0  ">We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work.</p>
+                                <p class="lead">We nominated:&nbsp;&nbsp;&nbsp;<small>
+
+                                    <abbr title="AAKS introduces the world to the weaving techniques of the women of Ghana, while creating sustainable jobs in Africa. Handcrafted in Ghana, A A K S creates bags in styles in bright and exuberant colours that maintain the spirit and durability of their ancestral  counterparts.">
+                                        <a href="https://www.aaksonline.com/" class="link">A A K S</a>
+                                    </abbr>
+
+                                        &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+
+                                    <abbr title="Studio-Lani creates handmade sculptural lighting and furniture pieces that celebrate West African elegance with a contemporary sensibility, working closely with various artisans in Nigeria.">
+                                            
+                                            <a href="http://studio-lani.com/" class="link">Studio Lani</a>
+                                        
+                                    </abbr>
+                                </small></p>
                             </div>
                         </div>
                     </div>
