0e0cb025ca9b75d2b325a07d64f6a327d7dc1658
jude okoroafor <judeokoroafor@gmail.com>
2021-01-12 16:14:17 +0100

    Update create-day-london-craft-week-2020.php

---
+++ projects/create-day-london-craft-week-2020.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<?php include '../inc/header.php' ?>
+
+<body class="">
+
+    <?php include '../inc/top-bar-2.php' ?>
+
+    <div class="container post-section">
+        <div class="row justify-content-md-center">
+            <div class="col col-sm-8 col-md-6">
+                <div class="row mb-3">
+                    <img src="/img/aaks-lani.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="row p-4 p-sm-0">
+                    <div class="col p-0">
+
+                    <span class="font2 lead">Project II</span>
+                    <h4 class="my-1">Create Day | London Craft Week</h4>
+                    <p class="font2 lead">2020</p>
+                    <p>We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work. </p>
+                    <p>We nominated:</p>
+                    <p class="font-weight-bold m-0">Studio Lani</p>
+                    <p>Studio-Lani creates handmade sculptural lighting and furniture pieces that celebrate West African elegance with a contemporary sensibility, working closely with various artisans in Nigeria. <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer"><br>www.studio-lani.com</a></p>
+                    <p class="font-weight-bold m-0">AAKS</p>
+                    <p>AAKS introduces the world to the weaving techniques of the women of Ghana, while creating sustainable jobs in Africa. Handcrafted in Ghana, A A K S creates bags in styles in bright and exuberant colours that maintain the spirit and durability of their ancestral counterparts. <a href="https://www.aaksonline.com/" target="_blank" rel="noopener noreferrer"><br>www.aaksonline.com</a></p>
+                    <p>We were excited about these brands particularly for their discovery and use local raw materials to manufacture and innovative, high quality products.</p>
+                    <p>Their brand maker videos are now up on <a href="www.createday.org" target="_blank" rel="noopener noreferrer">www.createday.org</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../inc/footer2.php' ?>
+
+
+</body>
+
+</html>
