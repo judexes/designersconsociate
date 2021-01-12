abcacdb29c1b9f39f44d4afb0f71ef8e42fe27fe
jude okoroafor <judeokoroafor@gmail.com>
2021-01-12 09:30:05 +0100

    Update the-designer-shirt-project-2018.php

--- projects/the-designer-shirt-project-2018.php
+++ projects/the-designer-shirt-project-2018.php
@@ -3,54 +3,28 @@
 
 <?php include '../inc/header.php' ?>
 
-<body class="single-project">
+<body class="">
 
     <?php include '../inc/top-bar-2.php' ?>
 
-    <section class="hero-section ">
-        <div class="container">
-            <div class="hero">
-                <div class="shade shade view-height-50 center-children">
-                    <div class="row justify-content-center align-items-center ">
-                        <div class="">
-                            <div class="text-center">
-                                <span class="title pt-sm-0  ">Project I</span>
-                            </div>
-                        </div>
+    <div class="container post-section">
+        <div class="row justify-content-md-center">
+            <div class="col col-sm-8 col-md-6">
+                <div class="row">
+                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                </div>
+                <div class="row p-4 p-sm-0">
+                    <div class="col p-0">
+
+                    <h6><span class="lead font2">Project I</span></h6>
+                    <h2 class="mb-0">The Designer Shirt Project</h2>
+                    <p class="lead font2 mb-2 mt-1">2018</p>
+                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-
-    <section class="project-section mt-5">
-        <div class="container view-height-100">
-            <div class="shade">
-                <div class="row ">
-                    <div class="col-sm-6 ">
-                        <div class="p-2">
-
-                            <img src="/img/NU-MI-DESIGN.jpg" alt="image" class="img-fluid">
-                        </div>
-                    </div>
-                    <div class="z-index-99">
-                        <div class="text-center">
-                            <span class="cursive p-3 title">Create Day | London Craft Week</span>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 z-index-75">
-                        <div class="p-2 pt-5">
-
-                            <div class="">
-                                <p class="lead pt-sm-0  ">We had the opportunity to nominate two African craft brands for Create Day 2020, a new global initiative from London Craft Week, celebrating the creative process of craftsmanship, the talented makers and their work.</p>
-                                <p class="lead">We nominated</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    </div>
 
     <?php include '../inc/footer2.php' ?>
 
