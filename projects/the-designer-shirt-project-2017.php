d9b4b0c2df335d344607570124cafd8e332ff30b
jude okoroafor <judeokoroafor@gmail.com>
2021-01-13 19:45:35 +0100

    Update the-designer-shirt-project-2017.php

--- projects/the-designer-shirt-project-2017.php
+++ projects/the-designer-shirt-project-2017.php
@@ -10,23 +10,106 @@
     <div class="container post-section">
         <div class="row justify-content-md-center">
             <div class="col col-sm-8 col-md-6">
-                <div class="row">
-                    <img src="/img/HCDC-6-edit1.jpg" alt="Image" class="img-fluid">
+                <div class="row mb-3">
+                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
                 </div>
                 <div class="row p-4 p-sm-0">
                     <div class="col p-0">
+                        <span class="font2 lead">Project II</span>
+                        <h4 class="my-1">Create The Designer Shirt Project</h4>
+                        <p class="font2 lead">2017 / 2018</p>
+                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
 
-                    <h6><span class="lead font2">Project I</span></h6>
-                    <h2 class="mb-0">The Designer Shirt Project</h2>
-                    <p class="lead font2 mb-2 mt-1">2017</p>
-                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+                        <div class="row icons mt-4 justify-content-center">
+
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g22.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Bridget Awosika</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g3076.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Vathiswa by Keji</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g3826.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g4108.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g4316.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Vathiswa by Keji</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g4622.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g4962.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g5260.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g5452.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-3 ">
+                                <div class="design-box">
+                                    <img src="/img/g5658.png" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Grey</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <?php include '../inc/footer2.php' ?>
+        <?php include '../inc/footer2.php' ?>
 
 
 </body>
