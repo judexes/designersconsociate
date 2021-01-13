e4cfeedfa311131d4c5d826879e04887c53c638d
jude okoroafor <judeokoroafor@gmail.com>
2021-01-13 19:45:25 +0100

    Update the-designer-shirt-project-2018.php

--- projects/the-designer-shirt-project-2018.php
+++ projects/the-designer-shirt-project-2018.php
@@ -15,17 +15,61 @@
                 </div>
                 <div class="row p-4 p-sm-0">
                     <div class="col p-0">
-                    <span class="font2 lead">Project II</span>
-                    <h4 class="my-1">Create The Designer Shirt Project</h4>
-                    <p class="font2 lead">2020</p>
-                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+                        <span class="font2 lead">Project II</span>
+                        <h4 class="my-1">Create The Designer Shirt Project</h4>
+                        <p class="font2 lead">2018</p>
+                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+
+
+                        <div class="row icons mt-4 justify-content-center">
+                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                                <div class="design-box">
+                                    <img src="/img/HCDC2.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Christie Brown</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                                <div class="design-box">
+                                    <img src="/img/HCDC9.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Meena</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                                <div class="design-box">
+                                    <img src="/img/HCDC14.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Tongoro</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                                <div class="design-box">
+                                    <img src="/img/HCDC16.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Clan</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                                <div class="design-box">
+                                    <img src="/img/HCDC6.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Tsemaye Binitie</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <?php include '../inc/footer2.php' ?>
+        <?php include '../inc/footer2.php' ?>
 
 
 </body>
