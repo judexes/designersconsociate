c6de1954da381914f7b181bc0be55f5ebdbb187a
jude okoroafor <judeokoroafor@gmail.com>
2021-01-13 11:19:00 +0100

    Update the-designer-shirt-project.php

--- projects/the-designer-shirt-project.php
+++ projects/the-designer-shirt-project.php
@@ -15,75 +15,200 @@
                 </div>
                 <div class="row p-4 p-sm-0">
                     <div class="col p-0">
-                    <span class="font2 lead">Project II</span>
-                    <h4 class="my-1">Create The Designer Shirt Project</h4>
-                    <p class="font2 lead">2020</p>
-                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+                        <span class="font2 lead">Project II</span>
+                        <h4 class="my-1">Create The Designer Shirt Project</h4>
+                        <p class="font2 lead">2020</p>
+                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
 
-                    <div class="row icons mt-4 justify-content-center">
+                        <div class="row icons mt-4 justify-content-center">
+
                             <div class=" col-6 col-sm-4 col-md-4 ">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Bridget Awosika</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Vathiswa by Keji</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">Vathiswa by Keji</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class=" col-6 col-sm-4 col-md-4">
-                                <div class="card p-0 mt-3">  
-                                    <div class="card-item">
-                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
-                                    </div>                
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="design-box">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <div class="m-sm-2 m-1">
+                                        <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Bridget Awosika</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Vathiswa by Keji</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Lisa Folawiyo</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Lisa Folawiyo</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Vathiswa by Keji</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Loza Maleombho</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Loza Maleombho</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Andrea Iyamah</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="box ">
+                                    <img src="/img/NU-MI-DESIGN.jpg" alt="Avatar" class="image img-fluid">
+                                    <a href="http://studio-lani.com/" target="_blank" rel="noopener noreferrer">
+                                        <div class="overlay">
+                                            <div class="m-sm-2 m-1">
+                                                <p class="m-0">By Andrea Iyamah</p>
+                                            </div>
+                                        </div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <?php include '../inc/footer2.php' ?>
+        <?php include '../inc/footer2.php' ?>
 
 
 </body>
