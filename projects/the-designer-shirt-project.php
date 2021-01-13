cac93be5d9e2ee6a299432a15767b3e417942d12
jude okoroafor <judeokoroafor@gmail.com>
2021-01-13 09:23:22 +0100

    Create the-designer-shirt-project.php

---
+++ projects/the-designer-shirt-project.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<?php include '../inc/header.php' ?>
+
+<body class="">
+
+    <?php include '../inc/top-bar-2.php' ?>
+
+    <div class="container post-section">
+        <div class="row justify-content-md-center">
+            <div class="col col-sm-8 col-md-6">
+                <div class="row mb-3">
+                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                </div>
+                <div class="row p-4 p-sm-0">
+                    <div class="col p-0">
+                    <span class="font2 lead">Project II</span>
+                    <h4 class="my-1">Create The Designer Shirt Project</h4>
+                    <p class="font2 lead">2020</p>
+                    <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
+
+                    <div class="row icons mt-4 justify-content-center">
+                            <div class=" col-6 col-sm-4 col-md-4 ">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                            <div class=" col-6 col-sm-4 col-md-4">
+                                <div class="card p-0 mt-3">  
+                                    <div class="card-item">
+                                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                                    </div>                
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../inc/footer2.php' ?>
+
+
+</body>
+
+</html>
