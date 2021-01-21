5df5249e08257ad15853e7a65a32a1ab9a260bea
jude okoroafor <judeokoroafor@gmail.com>
2021-01-21 19:17:19 +0100

    Update the-designer-shirt-project-2018.php

--- projects/the-designer-shirt-project-2018.php
+++ projects/the-designer-shirt-project-2018.php
@@ -20,7 +20,6 @@
                         <p class="font2 lead">2018</p>
                         <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
 
-
                         <div class="row icons mt-4 justify-content-center">
                             <div class=" col-6 col-sm-4 col-md-4 py-2">
                                 <div class="design-box">
