6cb35729ee226e3621c7a060913a64b0f905ec41
jude okoroafor <judeokoroafor@gmail.com>
2021-01-22 10:10:17 +0100

    Update the-designer-shirt-project.php

--- projects/the-designer-shirt-project.php
+++ projects/the-designer-shirt-project.php
@@ -18,10 +18,10 @@
                         <span class="font2 lead">Project II</span>
                         <h4 class="my-1">Create The Designer Shirt Project</h4>
                         <p class="font2 lead">2017 / 2018</p>
-                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. . Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
-
+                        <p>In 2017, we created and launched an exclusive designer initiative in Partnership with Lagos Fashion Week and the Human Capital Development Center (HCDC). </p>
+                        <p>Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. </p>
+                        <p>Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
                         <div class="row icons mt-4 justify-content-center">
-
                             <div class=" col-6 col-sm-4 col-md-3 ">
                                 <div class="design-box">
                                     <img src="/img/g22.png" alt="Avatar" class="image img-fluid">
