80f489498db594a9e783b62c66538d1506fd6d8e
jude okoroafor <judeokoroafor@gmail.com>
2021-01-22 10:43:58 +0100

    Update contact-us.php

--- sections/contact-us.php
+++ sections/contact-us.php
@@ -7,6 +7,15 @@
                         <div class="col">
                             <div class="hero-description text-center">
                                 <a class="title" href="mailto:hello@designersconsociate.org">hello@designersconsociate.org</a>
+                                
+                                <div class="socials mt-4">
+                                    <a href="https://www.linkedin.com/company/designers-consociate" target="_blank" rel="noopener noreferrer">
+                                        <img src="/img/linkedin1.svg" alt="" class="px-2 px-sm-0">
+                                        </a>
+                                    <a href="https://www.instagram.com/designers_consociate" target="_blank" rel="noopener noreferrer">
+                                        <img src="/img/instagram1.svg" alt="" class="px-2 px-sm-0">
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
