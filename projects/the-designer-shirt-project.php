6fe2483875335b385f134ede54b5182bbd90eb27
jude okoroafor <judeokoroafor@gmail.com>
2021-01-26 07:33:24 +0100

    Update the-designer-shirt-project.php

--- projects/the-designer-shirt-project.php
+++ projects/the-designer-shirt-project.php
@@ -9,9 +9,10 @@
 
     <div class="container post-section">
         <div class="row justify-content-md-center">
-            <div class="col col-sm-8 col-md-6">
+            <div class="col col-sm-8 col-md-8">
                 <div class="row mb-3">
-                    <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid">
+                    <!-- <img src="/img/HCDC-6-edit1a.jpg" alt="Image" class="img-fluid"> -->
+                    <img src="/img/img30.jpg" alt="Image" class="img-fluid">
                 </div>
                 <div class="row p-4 p-sm-0">
                     <div class="col p-0">
@@ -22,7 +23,134 @@
                         <p>Through the Designer Shirt Project, we promoted the manufacturing of garments in Africa, encouraged the use of locally and ethically sourced raw materials, and highlighted the work of Africa’s talented designers. </p>
                         <p>Over a two year period, 11 of Africa’s most sought after designers - Lisa Folawiyo, Christie Brown, Tongoro, Loza Maleombho, Bridget Awosika, Andrea Iyamah, Tsemaye Binitie, Meena, Clan, and Vathiswa by Keji crafted their take on a Classic Shirt, according to their brand's aesthetic. Manufactured in Nigeria by HCDC, an apparel manufacturing training center, these shirts were made in 100% Nigerian hand woven and hand dyed Cotton. Sold exclusively at the Lagos Fashion Week Retail Exhibit, the project was a resounding success as we saw a 90% sell through within the three day period.</p>
                         <div class="row icons mt-4 justify-content-center">
-                            <div class=" col-6 col-sm-4 col-md-3 ">
+
+                            <div class=" col-6 col-sm-6 col-md-6 ">
+                                <div class="design-box">
+                                    <div id="carouselControl1" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g22t.png" alt="First slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Bridget Awosika</p>
+                                                </div>
+                                            </div>  
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g3076t.png" alt="Second slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Vathiswa by Keji</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g3826t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g4108t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Lisa Folawiyo</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g4316t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Vathiswa by Keji</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g4622t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g4962t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Loza Maleombho</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g5260t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g5452t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Andrea Iyamah</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/g5658t.png" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">By Grey</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselControl1" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselControl1" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div class=" col-6 col-sm-6 col-md-6 py-2">
+                                <div class="design-box">
+                                    <div id="carouselControl2" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/HCDC2.jpg" alt="Third slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Christie Brown</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 image img-fluid" src="/img/HCDC9.jpg" alt="Second slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Meena</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item ">
+                                                <img class="d-block w-100 image img-fluid" src="/img/HCDC14.jpg" alt="First slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Tongoro</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item ">
+                                                <img class="d-block w-100 image img-fluid" src="/img/HCDC16.jpg" alt="First slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Clan</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100 image img-fluid" src="/img/HCDC6.jpg" alt="First slide">
+                                                <div class="m-sm-2 m-1">
+                                                    <p class="m-0 designer px-1">Tsemaye Binitie</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselControl2" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselControl2" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- <div class=" col-6 col-sm-4 col-md-3 ">
                                 <div class="design-box">
                                     <img src="/img/g22.png" alt="Avatar" class="image img-fluid">
                                     <div class="m-sm-2 m-1">
@@ -101,11 +229,12 @@
                                         <p class="m-0 designer px-1">By Grey</p>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
 
+
                         <div class="row icons mt-4 justify-content-center">
-                            <div class=" col-6 col-sm-4 col-md-4 py-2">
+                            <!-- <div class=" col-6 col-sm-4 col-md-4 py-2">
                                 <div class="design-box">
                                     <img src="/img/HCDC2.jpg" alt="Avatar" class="image img-fluid">
                                     <div class="m-sm-2 m-1">
@@ -144,7 +273,7 @@
                                         <p class="m-0 designer px-1">Tsemaye Binitie</p>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
 
                     </div>
