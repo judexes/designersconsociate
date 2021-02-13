fa66f4e4d045dd11b700ecf0b4de818e7a79f86d
jude okoroafor <judeokoroafor@gmail.com>
2021-02-13 01:44:49 +0100

    Hero animation 2

---
+++ index3.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    
+<?php include 'inc/header.php' ?>
+
+<body class="">
+
+    <?php include 'inc/top-bar.php' ?>
+
+    <?php include 'sections/home.hero3.php' ?>
+
+    <?php // include 'inc/footer.php' ?>
+
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/91297e957b.js" crossorigin="anonymous"></script> <!-- ICONS RESOURCE -->
+    <!-- ANIMATE ON SCROLL -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script src="javascript/vendors/jquery-3.5.1.slim.min.js"></script>
+    <script src="javascript/vendors/bootstrap.bundle.js"></script>
+    <script src="javascript/app.js"></script>
+
+</body>
+
+</html>
---
+++ sections/home.hero3.php
@@ -0,0 +1,34 @@
+    <!-- HERO SECTION -->
+    <section id="top" class="hero-section shade">
+        <div class="splash view-height-100 view-height-0-sm center-children center-children-0-sm">
+            <div class="container">
+                <div class="row pt-sm-0 justify-content-center align-items-center">
+                    <div class="col mt-sm-5">
+                        <div class="hero-description text-center">
+                            <!-- <h1 class="title pt-5 pt-sm-0 mb-3">Sustainable Solutions</h1> -->
+
+                            <div class="textcontainer">
+                                <span class="particletext bubbles">Sustainable Solutions</span>
+                            </div>
+
+                            <div class="col-md-8 description mt-1 offset-md-2">We are committed to developing sustainable solutions that invigorate the African Retail ecosystem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="textcontainer">
+                    <span class="particletext lines">Lines</span>
+                </div> -->
+
+                <div class="row pt-2 justify-content-center">
+                    <div class="col-auto">
+                        <div class="text-center">
+                            <a href="us.php">
+                                <span class="button">MORE</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
